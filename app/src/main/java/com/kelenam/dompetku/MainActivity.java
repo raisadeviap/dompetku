9fe4ff1a12c584f6642a7751978d55d15f7c9315
raisadeviap <raisadevi2150@gmail.com>
2025-04-17 21:59:55 +0700

    Raisa - work1 (#11)
    * dompetku
    
    * ini update dari beranda dan tambah menu
    
    * tambah statistik menu

--- app/src/main/java/com/kelenam/dompetku/MainActivity.java
+++ app/src/main/java/com/kelenam/dompetku/MainActivity.java
@@ -1,24 +1,52 @@
 package com.kelenam.dompetku;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.kelenam.dompetku.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Set listener ke BottomNavigationView
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        bottomNavigationView.setSelectedItemId(R.id.nav_beranda);
+
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    int id = item.getItemId();
+
+                    if (id == R.id.nav_beranda) {
+                        // Jika sudah di Beranda, tidak perlu melakukan apa-apa
+                        return true;
+                    } else if (id == R.id.nav_tambah) {
+                        startActivity(new Intent(MainActivity.this, TambahActivity.class));
+                        overridePendingTransition(0, 0); // Menghilangkan animasi transisi
+                        finish(); // Menutup MainActivity
+                        return true;
+                    } else if (id == R.id.nav_statistik) {
+                        startActivity(new Intent(MainActivity.this, StatistikActivity.class));
+                        overridePendingTransition(0, 0); // Menghilangkan animasi transisi
+                        finish(); // Menutup MainActivity
+                        return true;
+                    }
+
+                    return false;
+                }
+            };
 }
